1752d834834bd328398148889d73468bc96c2d79
Muhammad Rafi Triandi <education.triandy@gmail.com>
2022-04-08 20:28:55 +0700

    init-ticketing-apps

--- Belajar Pengembangan Aplikasi Android Intermediate/Advanced UI/TicketingApps/app/src/main/java/com/yandey/ticketingapps/MainActivity.kt
+++ Belajar Pengembangan Aplikasi Android Intermediate/Advanced UI/TicketingApps/app/src/main/java/com/yandey/ticketingapps/MainActivity.kt
@@ -2,10 +2,31 @@ package com.yandey.ticketingapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yandey.ticketingapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            finishButton.setOnClickListener {
+                seatsView.seat?.let {
+                    AppUtils.showToast(
+                        this@MainActivity,
+                        resources.getString(R.string.seat_number, it.name)
+                    )
+                } ?: run {
+                    AppUtils.showToast(
+                        this@MainActivity,
+                        resources.getString(R.string.please_choose_seat_first)
+                    )
+                }
+            }
+        }
     }
 }
