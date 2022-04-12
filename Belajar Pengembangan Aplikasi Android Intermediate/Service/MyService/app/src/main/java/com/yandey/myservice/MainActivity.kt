62938a5a1532da416df0d0b014dac2496ec08fe8
Muhammad Rafi Triandi <education.triandy@gmail.com>
2022-04-12 20:19:21 +0700

    add-bound-service

--- Belajar Pengembangan Aplikasi Android Intermediate/Service/MyService/app/src/main/java/com/yandey/myservice/MainActivity.kt
+++ Belajar Pengembangan Aplikasi Android Intermediate/Service/MyService/app/src/main/java/com/yandey/myservice/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.yandey.myservice
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import com.yandey.myservice.databinding.ActivityMainBinding
+import com.yandey.myservice.MyBoundService.MyBinder
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var mServiceBound = false
+    private lateinit var mBoundService: MyBoundService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +27,32 @@ class MainActivity : AppCompatActivity() {
             }
 
             btnStartBoundService.setOnClickListener {
+                val mBoundServiceIntent = Intent(this@MainActivity, MyBoundService::class.java)
+                bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE)
             }
 
             btnStopBoundService.setOnClickListener {
-
+                unbindService(mServiceConnection)
             }
+        }
+    }
+
+    private val mServiceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName) {
+            mServiceBound = false
+        }
+
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            val myBinder = service as MyBinder
+            mBoundService = myBinder.getService
+            mServiceBound = true
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mServiceBound) {
+            unbindService(mServiceConnection)
         }
     }
 }
