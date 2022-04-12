7651d658083a0da663d0124c7287639cc4c8f240
Muhammad Rafi Triandi <education.triandy@gmail.com>
2022-04-12 21:09:22 +0700

    init-my-sound

---
+++ Belajar Pengembangan Aplikasi Android Intermediate/Media/MySound/app/src/main/java/com/yandey/mysound/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.yandey.mysound
+
+import android.media.SoundPool
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.yandey.mysound.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var sp: SoundPool
+    private var soundId: Int = 0
+    private var spLoaded = false
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sp = SoundPool.Builder()
+            .setMaxStreams(10)
+            .build()
+
+        sp.setOnLoadCompleteListener { _, _, status ->
+            if (status == 0) {
+                spLoaded = true
+            } else {
+                Toast.makeText(this@MainActivity, "Gagal load", Toast.LENGTH_SHORT).show()
+            }
+        }
+        soundId = sp.load(this, R.raw.clinking_glasses, 1)
+
+        binding.btnPlay.setOnClickListener {
+            if (spLoaded) {
+                sp.play(soundId, 1f, 1f, 0, 0, 1f)
+            }
+        }
+    }
+}
